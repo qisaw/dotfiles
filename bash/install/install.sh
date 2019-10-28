49ed0988e4ebe5ac0116cb5b13c880619f060b75
Wasiq Kashkari <w.kashkari@outlook.com>
2019-10-28 13:35:34 +0100

    Install node and yarn

--- bash/install/install.sh
+++ bash/install/install.sh
@@ -2,6 +2,8 @@
  
 set -euo pipefail
 
+INTERNAL_NODE_VERSION=${NODE_VERSION_TO_INSTALL:=10}
+
 echo "============================================================="
 echo "                 Setting up config for bash                  "
 echo "============================================================="
@@ -9,6 +11,12 @@ echo "source ~/dotfiles/.bash_profile" >> ~/.bash_profile
 
 echo "=================== Sourcing bash_profile ==================="
 source ~/.bash_profile
+
+echo "====================== Installing Node ======================"
+nvm install ${INTERNAL_NODE_VERSION}
+
+echo "====================== Installing Yarn ======================"
+npm install -g yarn
 
 echo "============================================================="
 echo "            Finished! Your bash Setup is complete            "
