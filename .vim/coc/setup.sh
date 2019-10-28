fd71a20f3cae5f6c1164c3a1a8601e394496bfb1
Wasiq Kashkari <w.kashkari@outlook.com>
2019-10-28 11:07:28 +0100

    Remove comments

--- .vim/coc/setup.sh
+++ .vim/coc/setup.sh
@@ -3,17 +3,6 @@
 set -o nounset    # error when referencing undefined variable
 set -o errexit    # exit when command fails
 
-# Use package feature to install coc.nvim
-# If you want to use plugin manager, change DIR to plugin directory used by that manager.
-#DIR=~/.local/share/nvim/site/pack/coc/start
-# For vim user, the directory is different
-# DIR=~/.vim/pack/coc/start
-#mkdir -p $DIR
-#cd $DIR
-#git clone https://github.com/neoclide/coc.nvim.git --depth=1
-#cd $DIR/coc.nvim
-#./install.sh nightly
-
 # Install extensions
 mkdir -p ~/.config/coc/extensions
 cd ~/.config/coc/extensions
