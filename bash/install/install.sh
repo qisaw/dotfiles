391d99a8c10a2e821393981d34b3d779698b30a0
Wasiq Kashkari <w.kashkari@outlook.com>
2019-10-28 13:17:57 +0100

    Add a script to set up bash profile

---
+++ bash/install/install.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+ 
+set -euo pipefail
+
+echo "============================================================="
+echo "                 Setting up config for bash                  "
+echo "============================================================="
+echo "source ~/dotfiles/.bash_profile" >> ~/.bash_profile
+
+echo "=================== Sourcing bash_profile ==================="
+source ~/.bash_profile
+
+echo "============================================================="
+echo "            Finished! Your bash Setup is complete            "
+echo "============================================================="
